286531d353898c691d5e0cfb7bb1550cf8e78ed1
tinmaungzin <tinmaungzin.tmz@gmail.com>
2020-02-13 14:21:00 +0630

    some changes

--- resources/views/Layouts/sidebar.blade.php
+++ resources/views/Layouts/sidebar.blade.php
@@ -1,4 +1,5 @@
 @php $route= Route::currentRouteName() @endphp
+{{--@php dd($route); @endphp--}}
 
 <div class="sidebar-nb-mount shadow mr-0"  style="height: inherit;min-height: 100vh">
 
@@ -20,8 +21,10 @@
 {{--                </div>--}}
 {{--            </div>--}}
 
-            <li class="nav-item btn-mount mx-auto mt-2" id="member">
-                <a class="nav-link p-0  sidebar-box-mount img-member text-center  " href="{{url('member')}}">
+            <li class="nav-item btn-mount mx-auto mt-2 {{ in_array($route, ['member.index']) ? 'active2': ''}}"  id="member">
+                <a class="nav-link p-0  sidebar-box-mount  text-center img-member
+                    {{ in_array($route, ['member.index']) ? 'active-m': ''}}"
+                   href="{{url('member')}}">
                     <div class=" mt-0 px-auto "><p class="sb-text">Member</p></div>
                 </a>
             </li>
@@ -30,30 +33,44 @@
 {{--        pos=active-pos--}}
 
             {{----}}
-            <li class="nav-item btn-mount mx-auto " id="sr">
-                <a class="nav-link p-0  sidebar-box-mount img-sale-record text-center" href="{{url('sale')}}">
+            <li class="nav-item btn-mount mx-auto {{ in_array($route, ['sale.index']) ? 'active2': ''}} " id="sr">
+                <a class="nav-link p-0  sidebar-box-mount img-sale-record text-center {{ in_array($route, ['sale.index']) ? 'active-sr': ''}}" href="{{url('sale')}}">
                     <div class=" mt-0 px-auto "><p class="sb-text">Sale Record</p></div></a>
             </li>
 
 {{--        active-sr--}}
-            <li class="nav-item btn-mount mx-auto" id="stock">
-                <a class="nav-link p-0  sidebar-box-mount img-stock-inventory text-center" href="{{url('stock')}}">
+            <li class="nav-item btn-mount mx-auto {{ in_array($route, ['stock.index','stock.create','stock.transfer']) ? 'active2': ''}}" id="stock">
+                <a class="nav-link p-0  sidebar-box-mount img-stock-inventory text-center {{ in_array($route, ['stock.index','stock.create','stock.transfer']) ? 'active-si': ''}}" href="{{url('stock')}}">
                     <div class=" mt-0 px-auto "><p class="sb-text">Stock Inventory</p></div></a>
             </li>
 {{--        active-si--}}
-            <li class="nav-item btn-mount mx-auto" id="group">
-                <a class="nav-link p-0  sidebar-box-mount img-group text-center " href="{{url('currency_group')}}">
+            <li class="nav-item btn-mount mx-auto {{ in_array($route, ['currency_group.index']) ? 'active2': ''}}" id="group">
+                <a class="nav-link p-0  sidebar-box-mount img-group text-center {{ in_array($route, ['currency_group.index']) ? 'active-group': ''}}" href="{{url('currency_group')}}">
                     <div class=" mt-0 px-auto "><p class="sb-text">Currency Group</p></div></a>
             </li>
 {{--        active-group--}}
-            <li class="nav-item btn-mount mx-auto" id="daily">
-                <a class="nav-link p-0  sidebar-box-mount img-daily text-center " href="{{url('daily_currency')}}">
+            <li class="nav-item btn-mount mx-auto
+                {{ in_array($route, [
+                    'daily_currency.index.admin',
+                    'daily_currency.create.admin',
+                    'daily_currency.index.manager',
+                    'daily_currency.create.manager',
+                    'daily_currency.index.front',
+                    'daily_currency.create.front']) ? 'active2': ''}}" id="daily">
+                <a class="nav-link p-0  sidebar-box-mount img-daily text-center 
+                {{ in_array($route, [
+                    'daily_currency.index.admin',
+                    'daily_currency.create.admin',
+                    'daily_currency.index.manager',
+                    'daily_currency.create.manager',
+                    'daily_currency.index.front',
+                    'daily_currency.create.front']) ? 'active-daily': ''}}" href="{{url('daily_currency')}}">
                     <div class=" mt-0 px-auto "><p class="sb-text">Daily Currency</p></div></a>
             </li>
 
 {{--        active-daily--}}
-            <li class="nav-item btn-mount mx-auto" id="staff">
-                <a class="nav-link p-0  sidebar-box-mount img-staff text-center " href="{{url('staff')}}">
+            <li class="nav-item btn-mount mx-auto {{ in_array($route, ['staff.index']) ? 'active2': ''}}" id="staff">
+                <a class="nav-link p-0  sidebar-box-mount img-staff text-center {{ in_array($route, ['staff.index']) ? 'active-staff': ''}}" href="{{url('staff')}}">
                     <div class=" mt-0 px-auto "><p class="sb-text">Staff</p></div></a>
             </li>
             <li class="nav-item btn-mount mx-auto" id="branch">
